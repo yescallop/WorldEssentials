4fa60dccd575202cecf58888d933dedd44ede216
yescallop <s@ys0.me>
2016-04-08 19:31:32 +0800

    Small fix

--- src/main/java/cn/yescallop/worldessentials/WorldEssentials.java
+++ src/main/java/cn/yescallop/worldessentials/WorldEssentials.java
@@ -36,29 +36,25 @@ public class WorldEssentials extends PluginBase implements Listener {
         this.getLogger().info(lang.translateString("worldessentials.loaded"));
     }
 
-    @EventHandler
+    @EventHandler(ignoreCancelled = true)
     public void onPlayerTeleport(PlayerTeleportEvent event) {
-        if (!event.isCancelled()) {
-            Location from = event.getFrom();
-            Location to = event.getTo();
-            Player player = event.getPlayer();
-            if (!from.level.equals(to.level)) setPlayerInfos(player);
-            int gamemode = getPlayerGamemode(player, to.level);
-            player.setGamemode(gamemode);
-            player.getInventory().setContents(getPlayerInventoryContents(player, gamemode, to.level));
-        }
+        Location from = event.getFrom();
+        Location to = event.getTo();
+        Player player = event.getPlayer();
+        if (!from.level.equals(to.level)) setPlayerInfos(player);
+        int gamemode = getPlayerGamemode(player, to.level);
+        player.setGamemode(gamemode);
+        player.getInventory().setContents(getPlayerInventoryContents(player, gamemode, to.level));
     }
 
-    @EventHandler
+    @EventHandler(ignoreCancelled = true)
     public void onPlayerGameModeChange(PlayerGameModeChangeEvent event) {
-        if (!event.isCancelled()) {
-            Player player = event.getPlayer();
-            Config playerConfig = getPlayerConfig(player, player.getLevel());
+        Player player = event.getPlayer();
+        Config playerConfig = getPlayerConfig(player, player.getLevel());
             playerConfig.set("inventories", getPlayerInventories(player, player.getLevel()));
             playerConfig.save();
             int gamemode = event.getNewGamemode();
             player.getInventory().setContents(getPlayerInventoryContents(player, gamemode, player.getLevel()));
-        }
     }
 
     public BaseLang getLanguage() {
