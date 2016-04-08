cf084954802250ae3051b8d1e2dcdcd2be9ee8e3
yescallop <s@ys0.me>
2016-04-08 19:11:01 +0800

    Version 1.2.3

--- src/main/java/cn/yescallop/worldessentials/command/CommandManager.java
+++ src/main/java/cn/yescallop/worldessentials/command/CommandManager.java
@@ -1,5 +1,6 @@
 package cn.yescallop.worldessentials.command;
 
+import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandMap;
 
 import cn.yescallop.worldessentials.WorldEssentials;
@@ -7,12 +8,19 @@ import cn.yescallop.worldessentials.command.defaults.SetWorldGamemodeCommand;
 import cn.yescallop.worldessentials.command.defaults.SpawnCommand;
 import cn.yescallop.worldessentials.command.defaults.TpwCommand;
 
+import java.util.ArrayList;
+
 public class CommandManager {
 
     public static void registerAll(WorldEssentials plugin) {
         CommandMap map = plugin.getServer().getCommandMap();
-        map.register("worldessentials", new SetWorldGamemodeCommand(plugin));
-        map.register("worldessentials", new SpawnCommand(plugin));
-        map.register("worldessentials", new TpwCommand(plugin));
+        ArrayList<Command> commands = new ArrayList<Command>(){
+            {
+                add(new SetWorldGamemodeCommand(plugin));
+                add(new SpawnCommand(plugin));
+                add(new TpwCommand(plugin));
+            }
+        };
+        map.registerAll("worldessentials", commands);
     }
 }
